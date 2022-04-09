37d5a3392d987b87f1a6cb18fc0a4f5b0f1c20ac
Grace <helen.g.anderson@me.com>
2022-04-09 13:23:35 +1000

    added buttons to home and contact pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-//
+// sticky navbar
+// window.onscroll = function() {stickyNavbar()};
+
+// var navbar = document.querySelector('#search-form');
+
+
+//button link to contact page
+var buttonLetsWork = document.querySelector('#lets-work-button');
+var buttonGoHome = document.querySelector('#go-home-button');
+
+var buttonLetsWorkClickHandler = function (event) {
+    event.preventDefault();
+    document.location='contact.html'
+}
+
+var buttonGoHomeClickHandler = function (event) {
+    event.preventDefault();
+    document.location='index.html'
+}
+
+buttonLetsWork.addEventListener('click', buttonLetsWorkClickHandler);
+
+buttonGoHome.addEventListener('click', buttonGoHomeClickHandler);
