962c3ec35759c7e7f44d6f260f1ace0bb2f74ab7
Grace <helen.g.anderson@me.com>
2022-04-09 13:50:47 +1000

    completed button js and added smoothing page transition

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,36 @@
 
 // var navbar = document.querySelector('#search-form');
 
+//page transition 
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 500)
+}
 
-//button link to contact page
-var buttonLetsWork = document.querySelector('#lets-work-button');
-var buttonGoHome = document.querySelector('#go-home-button');
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
+})
+
+//button links between home and contact pages
+var buttonLetsWork = document.querySelector("#lets-work-button");
+var buttonGoHome = document.querySelector("#go-home-button");
 
 var buttonLetsWorkClickHandler = function (event) {
-    event.preventDefault();
-    document.location='contact.html'
-}
+  event.preventDefault();
+  document.location = "contact.html";
+};
 
 var buttonGoHomeClickHandler = function (event) {
-    event.preventDefault();
-    document.location='index.html'
+  event.preventDefault();
+  document.location = "index.html";
+};
+
+if (buttonLetsWork) {
+  buttonLetsWork.addEventListener("click", buttonLetsWorkClickHandler);
 }
 
-buttonLetsWork.addEventListener('click', buttonLetsWorkClickHandler);
-
-buttonGoHome.addEventListener('click', buttonGoHomeClickHandler);
+if (buttonGoHome) {
+  buttonGoHome.addEventListener("click", buttonGoHomeClickHandler);
+}
